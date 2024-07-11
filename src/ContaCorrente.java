e36de8260f4e0da82777321711fc7c7edba23b90
Guilherme Augusto <guilhermesaldanha08@gmail.com>
2024-07-10 23:50:55 -0300

    feat: encapsulate

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -3,6 +3,30 @@ public class ContaCorrente {
     private int numero;
     private double saldo;
 
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
     public void sacar(){
 
     }
@@ -15,3 +39,4 @@ public class ContaCorrente {
 
     }
 }
+
--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -3,6 +3,30 @@ public class ContaPoupanca {
     private int numero;
     private double saldo;
 
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
     public void sacar(){
 
     }
